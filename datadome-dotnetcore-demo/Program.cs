3751f998421044b06ba9c8dd77317281bfb4b932
lauro <111757181+laurodd@users.noreply.github.com>
2024-07-19 09:04:05 +0200

    feat: add initial version (#1)

---
+++ datadome-dotnetcore-demo/Program.cs
@@ -0,0 +1,15 @@
+﻿using DataDome.Configuration;
+using DataDome;
+
+//minimal demo webapp based on : https://learn.microsoft.com/fr-fr/visualstudio/mac/asp-net-core?view=vsmac-2022
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.Configure<DataDomeConfig>(builder.Configuration.GetSection("DataDomeConfiguration"));
+
+var app = builder.Build();
+
+app.UseDataDome();
+
+app.MapGet("/", () => "Hello ASP.NET Core!");
+
+app.Run();
